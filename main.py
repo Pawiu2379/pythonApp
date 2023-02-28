e5cf1016947b95ffd1cd33a29e015b68763f49aa
Paweł__ <pawelkoc681@gmail.com>
2023-02-28 23:48:56 +0100

    odc 2

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import tkinter as tk
 
 window = tk.Tk()
 
+w = tk.Label(window,text='1')
+w.place(x=0,y=0)
+w = tk.Label(window,text='2')
+w.place(x=50,y=0)
+w = tk.Label(window,text='3')
+w.place(x=100,y=0)
 window.mainloop()
