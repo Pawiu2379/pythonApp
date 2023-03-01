a5605e650d9765ae668647ac8e5d11cceb3a5741
Paweł__ <pawelkoc681@gmail.com>
2023-03-01 20:55:00 +0100

    odc 4

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import tkinter as tk
 
 window = tk.Tk()
+frame = tk.Frame(background='black')
+frame.pack(side=tk.TOP)
 
-for x in range(3):
-    for y in range(3):
-        label = tk.Label(window,text=f"Row {x}\n Column {y}",relief=tk.SOLID,borderwidth=1)
-        label.grid(row=x,column=y)
-
-
-label = tk.Label(window,text="this is a span ",relief=tk.RAISED,borderwidth=1)
-label.grid(row=3,column=8,columnspan=3)
+w = tk.Label(frame, text='label in frame')
+w.pack(pady=5,padx=5)
 window.mainloop()
